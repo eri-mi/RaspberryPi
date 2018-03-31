995bbbf1df5e378d8e399115b4500cfb74cf564d
miya789 <miya.nori42395@gmail.com>
2018-03-31 23:24:07 +0900

    Added test

--- mlbb/ml-04-04-2feat3class.py
+++ mlbb/ml-04-04-2feat3class.py
@@ -16,9 +16,9 @@ y = iris.target
 X = X[:,:2]
 
 # 分類用にサポートベクトルマシンを用意
-clf = svm.SVC(C=1.0, kernel='linear', decision_function_shape='ovr')
+# clf = svm.SVC(C=1.0, kernel='linear', decision_function_shape='ovr')
 # 'auto'を指定すると1/(次元)がセットされる。この場合、1/2=0.5
-#clf = svm.SVC(C=1.0, kernel='rbf', gamma='auto', decision_function_shape='ovr')
+clf = svm.SVC(C=1.0, kernel='rbf', gamma='auto', decision_function_shape='ovr')
 # gammaを大きくすると、曲率が大きい（よく曲がる）境界となる
 #clf = svm.SVC(C=1.0, kernel='rbf', gamma=1.0, decision_function_shape='ovr')
 # データに最適化
